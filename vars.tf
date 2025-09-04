b5ad7df27d0cf2cb7eeefeace12fa7fa24938494
KIRAN <ks@gmail.com>
2025-09-04 06:51:48 +0530

    rabbitmq

--- vars.tf
+++ vars.tf
@@ -1,4 +1,6 @@
-#variable "env" {}
-#variable "name" {}
-#variable "subnets" {}
-#variable "instance_type" {}
+variable "env" {}
+variable "name" {}
+variable "subnets" {}
+variable "instance_type" {}
+variable "min_size" {}
+variable "max_size" {}
