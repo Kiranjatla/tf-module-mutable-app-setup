ec5347c58a7ce7bc78128aea06490b2a8257709f
KIRAN <ks@gmail.com>
2025-09-04 06:54:07 +0530

    autoscaling group creation with launch template

--- asg.tf
+++ asg.tf
@@ -16,7 +16,7 @@ resource "aws_autoscaling_group" "asg" {
     version = "$Latest"
   }
 }
-tag {
+tag =  {
   key                 = "Name"
   value               = "${var.env}-${var.name}"
   propagate_at_launch = true
