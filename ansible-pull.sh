622d38d76ea61a928069be3a587b25e0dfcf049e
KIRAN <ks@gmail.com>
2025-09-06 17:05:27 +0530

    auto scaling group

--- ansible-pull.sh
+++ ansible-pull.sh
@@ -3,4 +3,4 @@
 LOG=/opt/user-data-$$.log
 yum install python39-devel -y &>>$LOG
 pip3.9 install ansible botocore boto3 &>>$LOG
-ansible-pull -i localhost, -U https://github.com/raghudevopsb67/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${ENV} &>>$LOG
+ansible-pull -i localhost, -U https://github.com/kiranjatla9/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${ENV} &>>$LOG
