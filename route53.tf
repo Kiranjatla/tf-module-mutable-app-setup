596fa166df6293aea7aa657493effe2f7bf1a1ce
KIRAN <ks@gmail.com>
2025-09-12 19:50:47 +0530

    Load Balancers with target groups

--- route53.tf
+++ route53.tf
@@ -1,8 +1,12 @@
-#resource "aws_route53_record" "private" {
-#  count   = var.type == "backend" ? 1 : 0
-#  zone_id = var.private_zone_id
-#}
-#
+resource "aws_route53_record" "private" {
+  count   = var.type == "backend" ? 1 : 0
+  zone_id = var.private_zone_id
+  name = "${var.name}-${var.env}-asg"
+  type = "CNAME"
+  ttl = 300
+  records = [var.alb["private"].lb_dns_name]
+}
+
 #resource "aws_route53_record" "public" {
 #  count   = var.type == "frontend" ? 1 : 0
 #  zone_id = var.public_zone_id
--- vars.tf
+++ vars.tf
@@ -9,8 +9,8 @@ variable "BASTION_NODE" {}
 variable "app_port_no" {}
 variable "vpc_cidr" {}
 #variable "PROMETHEUS_NODE" {}
-#variable "alb" {}
-#variable "private_zone_id" {}
+variable "alb" {}
+variable "private_zone_id" {}
 #variable "lb_listener_priority" {}
 #variable "type" {}
 #variable "public_zone_id" {}
