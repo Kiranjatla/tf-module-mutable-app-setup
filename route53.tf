6cb18fefb984d615bb509d8b8f4274455befe95a
KIRAN <ks@gmail.com>
2025-09-12 19:51:52 +0530

    Load Balancers with target groups

--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "private" {
-  count   = var.type == "backend" ? 1 : 0
+#  count   = var.type == "backend" ? 1 : 0
   zone_id = var.private_zone_id
   name = "${var.name}-${var.env}-asg"
   type = "CNAME"
