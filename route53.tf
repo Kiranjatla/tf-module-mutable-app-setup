cb32385894b470ca58f44262c3e489cc9f5c5eb9
KIRAN <ks@gmail.com>
2025-09-12 19:54:39 +0530

    route53

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "private" {
 #  count   = var.type == "backend" ? 1 : 0
   zone_id = var.private_zone_id
-  name = "${var.name}-${var.env}-asg"
+  name = "${var.name}-${var.env}"
   type = "CNAME"
   ttl = 300
   records = [var.alb["private"].lb_dns_name]
