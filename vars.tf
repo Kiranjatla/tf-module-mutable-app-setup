768539c4fcb1fd1f3fc011698b91f2e123b45278
KIRAN <ks@gmail.com>
2025-09-12 21:45:22 +0530

    Listener rule

--- vars.tf
+++ vars.tf
@@ -11,8 +11,8 @@ variable "vpc_cidr" {}
 #variable "PROMETHEUS_NODE" {}
 variable "alb" {}
 variable "private_zone_id" {}
-#variable "lb_listener_priority" {}
-#variable "type" {}
+variable "lb_listener_priority" {}
+variable "type" {}
 #variable "public_zone_id" {}
 #variable "public_dns_name" {}
 #variable "ACM_ARN" {}
